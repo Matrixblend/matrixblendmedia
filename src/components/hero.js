f7b48d25b8c01b3a3ae1f750d2c1e251421ec8e3
Josh Head <jhead12@users.noreply.github.com>
2019-07-14 12:57:23 -0700

    w/o hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,19 +4,7 @@ import Img from 'gatsby-image'
 
 export default ({ data }) => (
   <section id="banner">
-									<div className="content">
-										<header>
-                    <h1>{data.name}</h1>
-											<p>{data.title}</p>
-										</header>
-										<p>{data.shortBio.shortBio}</p>
-										<ul className="actions">
-											<li><a href="#" className="button big">Learn More</a></li>
-										</ul>
-									</div>
-									<span className="image object">
-                  <Img  alt={data.name} sizes={data.heroImage.sizes} />
-									</span>
+							
 	  </section>
 
 )
