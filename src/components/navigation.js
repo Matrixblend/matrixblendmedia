70bad0aace44d855e0a043d49aae9e6f0d4a4654
Josh Head <jhead12@users.noreply.github.com>
2019-07-13 23:04:31 -0700

    build test

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 // import styles from './navigation.module.css'
-import styles from './main.css'
+// import styles from './main.css'
 
 
 export default () => (
