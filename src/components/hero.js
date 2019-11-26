dcca40dd51f8b05a327fd780bb763f645ee9ddfd
Josh Head <jhead12@users.noreply.github.com>
2019-11-26 10:17:44 -0800

    hero fix

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,7 +4,7 @@ import classNames from "classnames"
 import Img from 'gatsby-image'
 import Link from 'gatsby-link'
 import '../jss/material-kit-react/components/parallaxStyle'
-import Parallax from './Parallex/Parallax'
+// import Parallax from './Parallex/Parallax'
 import videoSrc from "../videos/logoVideo.mp4"
 
 
@@ -30,7 +30,7 @@ export default ({ data }) => (
 					</ul>
 				</div>
 				<span className="image object">
-				{/* <Img  alt={data.name} sizes={data.heroImage.sizes} /> */}
+				<Img  alt={data.name} sizes={data.heroImage.sizes} />
 				</span>
 				{/* <!-- Calendly link widget begin --> */}
 
